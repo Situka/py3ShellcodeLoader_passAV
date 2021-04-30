58d58482d6ec0a78cb1fc983d22b26686b2b81f0
U2l0dWth <46290880+Situka@users.noreply.github.com>
2021-04-30 14:22:46 +0800

    Add files via upload

---
+++ cha20.py
@@ -0,0 +1,35 @@
+from Crypto.Cipher import ChaCha20
+from Crypto.Random import get_random_bytes
+import base64
+import random
+import sys
+import codecs
+import json
+
+def encrypt(text,key):
+    key = key.encode('utf-8')
+    cipher = ChaCha20.new(key=key)
+    ciphertext = cipher.encrypt(text)
+    nonce = base64.b64encode(cipher.nonce.hex().encode('utf-8'))
+    cshell = base64.b64encode(ciphertext.hex().encode('utf-8'))
+    return [str(nonce,'utf-8'),str(cshell,'utf-8'),ciphertext,cipher.nonce]
+    #return ciphertext
+
+def getkey():
+    randstr = random.sample('qazwsxedcrfvtgbyhnujmikolpQAZWSXEDCRFVTGBYHNUJMIKOLP1234567890',32)
+    key = (''.join(randstr))
+    return key
+
+shellcode = sys.argv[1]
+deshellcode = codecs.escape_decode(shellcode)[0]
+hexshellcode = deshellcode.hex()
+enshellcode = base64.b64encode(hexshellcode.encode('utf-8'))
+#ccode = add_16(enshellcode)
+key = getkey()
+cshell = encrypt(enshellcode,key)
+jsonpost = json.dumps({'key':key,'nonce':cshell[0],'ciphertext':cshell[1]})
+print(jsonpost)
+print('-----------------------------------------------------------')
+print(cshell[2])
+print('-----------------------------------------------------------')
+print(cshell[3])
